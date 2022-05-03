31a067546850a45bb611222c7798e363a62e31d1
Shirokuma <ikuyama_kyo@outlook.jp>
2022-05-04 02:28:20 +0900

    5/4 2:28

---
+++ src/main/java/com/github/mckumama3/deathnoteplugin/deathnoteplugin/Commands/GameStartCommandHandler.java
@@ -0,0 +1,26 @@
+package com.github.mckumama3.deathnoteplugin.deathnoteplugin.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class GameStartCommandHandler implements CommandExecutor
+{
+
+    @Override
+    public boolean onCommand(
+            CommandSender sender,
+            Command command,
+            String label,
+            String[] args)
+    {
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/com/github/mckumama3/deathnoteplugin/deathnoteplugin/DeathNotePlugin.java
@@ -0,0 +1,11 @@
+package com.github.mckumama3.deathnoteplugin.deathnoteplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathNotePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //
+    }
+}
