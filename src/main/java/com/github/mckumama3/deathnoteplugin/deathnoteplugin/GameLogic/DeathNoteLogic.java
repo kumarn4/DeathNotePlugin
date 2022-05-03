4cd36da0c65ee96359127beb5b3999c24f916551
Shirokuma <ikuyama_kyo@outlook.jp>
2022-05-04 02:54:59 +0900

    5/4 2:54

--- src/main/java/com/github/mckumama3/deathnoteplugin/deathnoteplugin/GameLogic/DeathNoteLogic.java
+++ src/main/java/com/github/mckumama3/deathnoteplugin/deathnoteplugin/GameLogic/DeathNoteLogic.java
@@ -3,10 +3,17 @@ package com.github.mckumama3.deathnoteplugin.deathnoteplugin.GameLogic;
 import org.bukkit.entity.Player;
 
 import java.util.Collection;
+import java.util.Random;
 
 public class DeathNoteLogic
 {
+    Collection<? extends Player> Players;
     public DeathNoteLogic(Collection<? extends Player> players){
+        Players = players;
+    }
 
+    public Player ChooseKiller(){
+        int i = new Random().nextInt(Players.size());
+        return Players.stream().toList().get(i);
     }
 }
